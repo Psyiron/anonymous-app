1b417cbfc7815a3dae84e7121b047f47e3a2d1b2
Psyiron <edjb19palima@gmail.com>
2026-05-20 23:55:23 +0800

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,37 +1,49 @@
-const API = "https://your-backend-url.onrender.com";
-let token = null;
+let token = localStorage.getItem("token");
 
-function login() {
+async function login() {
   const password = document.getElementById("password").value;
 
-  fetch(`${API}/admin/login`, {
+  const res = await fetch("/admin/login", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify({ password })
-  })
-  .then(res => res.json())
-  .then(data => {
-    token = data.token;
-
-    document.getElementById("loginBox").style.display = "none";
-    document.getElementById("dashboard").style.display = "block";
   });
+
+  const data = await res.json();
+
+  if (data.token) {
+    token = data.token;
+    localStorage.setItem("token", token);
+    alert("Login successful!");
+    loadMessages();
+  } else {
+    alert("Wrong password");
+  }
 }
 
-function loadMessages() {
-  fetch(`${API}/messages`, {
-    headers: { Authorization: token }
-  })
-  .then(res => res.json())
-  .then(data => {
-    const box = document.getElementById("messages");
-    box.innerHTML = "";
+async function loadMessages() {
+  if (!token) {
+    alert("Please login first");
+    return;
+  }
 
-    data.reverse().forEach(msg => {
-      const div = document.createElement("div");
-      div.className = "message-box";
-      div.innerText = msg.message;
-      box.appendChild(div);
-    });
+  const res = await fetch("/messages", {
+    headers: {
+      "Authorization": token
+    }
+  });
+
+  const data = await res.json();
+
+  const container = document.getElementById("messages");
+  container.innerHTML = "";
+
+  data.forEach(msg => {
+    const div = document.createElement("div");
+    div.className = "msg";
+    div.innerText = `${msg.date} - ${msg.message}`;
+    container.appendChild(div);
   });
 }
